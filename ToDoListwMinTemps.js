cddcd7ac0dc5388294a2903a2c143206e90a1675
BenjaminF <fridkisb@oregonstate.edu>
2018-03-23 16:55:11 -0500

    commit

--- ToDoListwMinTemps.js
+++ ToDoListwMinTemps.js
@@ -27,7 +27,6 @@ app.get('/',function(req,res,next){
   context.user = req.session.name;
   context.toDoItemCount = req.session.toDoItems.length || 0;
   context.toDoItems = req.session.toDoItems || [];
-  //console.log(context.toDo);
   if(context.toDoItemCount === 1){
 	context.singular = true;
   }
@@ -50,17 +49,13 @@ app.post('/toDoList',function(req,res,next){
   }
 
   if(req.body.itemToAdd){
-	//console.log(req.body.minTemp);
     req.session.toDoItems.push({"ItemName":req.body.itemToAdd, "id":req.session.curId, 
 									"city": req.body.cityDesignation, "Minimum Temp":
 									Number(req.body.minTemp)});
     req.session.curId++;
-	//console.log(openWMURL+req.session.toDoItems[req.session.toDoItems.length - 1].city
-	//	+openWMCreds.openWMKey+'&units=imperial');
 	request(openWMURL+req.session.toDoItems[req.session.toDoItems.length - 1].city
 		+openWMCreds.openWMKey+'&units=imperial', function(err, response, body){
 	  if(!err & response.statusCode < 400){
-		  //console.log(JSON.parse(body).main.temp);
 		  req.session.toDoItems[req.session.toDoItems.length - 1].temp = Number(JSON.parse(body).main.temp);
 		  if(req.session.toDoItems[req.session.toDoItems.length - 1].temp >= 
 			 req.session.toDoItems[req.session.toDoItems.length - 1]["Minimum Temp"]){
@@ -79,20 +74,14 @@ app.post('/toDoList',function(req,res,next){
 					}, function(err, response, body){
 						if(!err && response.statusCode < 400){
 							context.externalPostRequest = JSON.parse(body).json;
-							//console.log(req.body.externalPostRequestContent);
-							//console.log(context.externalPostRequest);
 							context.user = req.session.name;
 						    context.toDoItemCount = req.session.toDoItems.length;
 						    context.toDoItems = req.session.toDoItems;
-						    //console.log(context.toDoItems);
-						    //var city = req.body.cityDesignation;
 							if(context.toDoItemCount === 1){
-								console.log('yippie!');
 								res.render('toDoListSingular',context);
 								return;
 							}
 							else{
-								console.log('yippie!!');
 								res.render('toDoList', context);
 								return;
 							}
@@ -117,14 +106,11 @@ app.post('/toDoList',function(req,res,next){
   else{	
 	  if(req.body.done){
 		req.session.toDoItems = req.session.toDoItems.filter(function(e){
-		  //console.log(e.atMinTemp, e.id, Number(req.body.id));
-		  //console.log(e.atMinTemp === true && e.id === Number(req.body.id) ? false : true);
 		  return e.atMinTemp === true && e.id === Number(req.body.id) ? false : true;
 		});
 		  context.user = req.session.name;
 		  context.toDoItemCount = req.session.toDoItems.length;
 		  context.toDoItems = req.session.toDoItems;
-		  //console.log(context.toDoItems);
 		  if(context.toDoItemCount === 1){
 			res.render('toDoListSingular',context);
 			return
@@ -137,7 +123,6 @@ app.post('/toDoList',function(req,res,next){
 	  context.user = req.session.name;
 	  context.toDoItemCount = req.session.toDoItems.length;
 	  context.toDoItems = req.session.toDoItems;
-	  //console.log(context.toDoItems);
 	  if(context.toDoItemCount === 1){
 		res.render('toDoListSingular',context);
 		}
